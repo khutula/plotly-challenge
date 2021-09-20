6fa8f104d24b800cd94d92ca8d6ace39761b7c3d
Karina Hutula <khutula@g.clemson.edu>
2021-09-20 10:55:12 -0400

    add code to create bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -26,23 +26,29 @@ d3.select("select").on("change", function() {
 
 function makePlot(index) {
     d3.json(url).then(function(data) {
-        let values = data.samples[index].sample_values.slice(0,10).reverse();
+        let values = data.samples[index].sample_values;
 
-        let otuIDs = data.samples[index].otu_ids.slice(0,10).map(function(item) {
+        let valueSlice = values.slice(0,10).reverse();
+
+        let otuIDs = data.samples[index].otu_ids;
+
+        let otuIDStrings = otuIDs.slice(0,10).map(function(item) {
             return `OTU ${item}`
         }).reverse();
 
-        let labels = data.samples[index].otu_labels.slice(0,10).reverse();
+        let labels = data.samples[index].otu_labels;
 
-        let traceData = [{
-            x: values,
-            y: otuIDs,
-            text: labels,
+        let labelSlice = labels.slice(0,10).reverse();
+
+        let traceBar = [{
+            x: valueSlice,
+            y: otuIDStrings,
+            text: labelSlice,
             type: "bar",
             orientation: "h"
         }];
 
-        Plotly.newPlot("bar", traceData)
+        Plotly.newPlot("bar", traceBar)
 
         let demographic = data.metadata[index];
         
@@ -50,10 +56,24 @@ function makePlot(index) {
 
         demoInfo.selectAll("span").remove();
         demoInfo.selectAll("br").remove();
-        
+
         for (let i=0; i<Object.entries(demographic).length; i++) {
             demoInfo.append("span").text(`${Object.keys(demographic)[i]}: ${Object.values(demographic)[i]}`).append("br");
         };
+
+        let traceBubble = [{
+            x: otuIDs,
+            y: values,
+            text: labels,
+            mode: "markers",
+            marker: {
+                colorscale: 'Picnic',
+                color: otuIDs,
+                size: values
+            }
+        }];
+
+        Plotly.newPlot("bubble", traceBubble)
     });
 };
 
